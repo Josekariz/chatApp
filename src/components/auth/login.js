67574bb29d6bfca344a344da655b268c94c4b685
Joseph <sejokarizz@gmail.com>
2023-05-01 10:23:08 +0300

    login page with chakra ui styling

---
+++ src/components/auth/login.js
@@ -0,0 +1,74 @@
+import {
+  Box,
+  Button,
+  Center,
+  FormControl,
+  //   FormErrorMessage,
+  FormLabel,
+  Heading,
+  Input,
+  Link,
+  Text,
+} from "@chakra-ui/react";
+import { REGISTER } from "lib/routes";
+import { Link as RouterLink } from "react-router-dom";
+
+export function Login() {
+  return (
+    <Center w="100%" h="100vh">
+      <Box mx="1" maxW="md" p="9" borderWidth="1px" boderRadius="lg">
+        <Heading mb="4" size="lg" textAlign="center">
+          Log in
+        </Heading>
+        <form onSubmit={() => {}}>
+          <FormControl
+            //</form>isInvalid={true}
+            py="2"
+          >
+            <FormLabel>Gmail</FormLabel>
+            <Input type="email" placeholder="user@gmail.com" />
+            {/* <FormErrorMessage>This is an error message</FormErrorMessage> */}
+          </FormControl>
+
+          {/* password here */}
+          <FormControl
+            //</form>isInvalid={true}
+            py="2"
+          >
+            <FormLabel>Password</FormLabel>
+            <Input type="password" placeholder="********" />
+            {/* <FormErrorMessage>This is an error message</FormErrorMessage> */}
+          </FormControl>
+
+          {/* submit button */}
+
+          <Button
+            mt="4"
+            type="submit"
+            colorScheme="pink"
+            size="md"
+            w="full"
+            isLoading={true}
+            loadingText="Logging in"
+          >
+            Log In
+          </Button>
+        </form>
+        <Text fontSize="xlg" align="center" mt="6">
+            Don't have an account?{"  "}
+          <Link
+            as={RouterLink}
+            to={REGISTER}
+            color="pink.800"
+            fontWeight="medium"
+            textDecor="underline"
+            _hover={{ background: "pink.100" }}
+          >
+            Register
+          </Link>
+          {"  "}instead!
+        </Text>
+      </Box>
+    </Center>
+  );
+}
